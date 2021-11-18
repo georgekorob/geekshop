f43e19910fa32077385e0b309038313f0bf4e16c
George <georgekorob@gmail.com>
2021-11-18 14:34:11 +0300

    split urls

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from mainapp.views import index, products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products'),
+    path('products/', include('mainapp.urls',namespace='mainapp')),
+    # path('products/', products, name='products'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ mainapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from mainapp.views import index, products
+
+app_name = 'products'
+urlpatterns = [
+    path('', products, name='products'),
+]
