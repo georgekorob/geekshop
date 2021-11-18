2682f92ed3e3b532a76af95c9c9a83a30e702efc
George <georgekorob@gmail.com>
2021-11-18 12:44:16 +0300

    make migrations

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-with open('../key.txt', 'r') as f:
+with open('key.txt', 'r') as f:
     sectet_key_django = f.read()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = f'django-insecure-{sectet_key_django}'
