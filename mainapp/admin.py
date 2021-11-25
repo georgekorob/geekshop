f1c7c49b1a31954e7ebe2ab46aa0a8e338663a7d
George <georgekorob@gmail.com>
2021-11-25 15:39:16 +0300

    add template basket

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from mainapp.models import ProductCategory,Product
+from mainapp.models import ProductCategory, Product
 
 admin.site.register(ProductCategory)
 admin.site.register(Product)
