a406571576c803db3e8e470f7e1cf8a8b6226ae9
georgekorob <georgekorob@gmail.com>
2021-12-04 14:28:25 +0300

    update users

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from authapp.forms import UserRegisterForm
+from authapp.forms import UserRegisterForm, UserProfileForm
 from authapp.models import User
 
 
@@ -14,6 +14,20 @@ class UserAdminRegisterForm(UserRegisterForm):
     def __init__(self, *args, **kwargs):
         super(UserAdminRegisterForm, self).__init__(*args, **kwargs)
 
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+        self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+
+class UserAdminProfileForm(UserProfileForm):
+    email = forms.EmailField(widget=forms.EmailInput())
+    username = forms.CharField(widget=forms.TextInput())
+
+    def __init__(self, *args, **kwargs):
+        super(UserAdminProfileForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs['readonly'] = False
+        self.fields['username'].widget.attrs['readonly'] = False
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
         self.fields['image'].widget.attrs['class'] = 'custom-file-input'
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,11 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-
 # Create your views here.
 from django.urls import reverse
 
-from adminapp.forms import UserAdminRegisterForm
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 from authapp.models import User
 
 
@@ -17,7 +16,7 @@ def create_user(request):
     if request.method == 'POST':
         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
         if form.is_valid():
-            form.save() #fdkebov427hflk
+            form.save()  # fdkebov427hflk
             return HttpResponseRedirect(reverse('adminapp:read_users'))
     else:
         form = UserAdminRegisterForm()
@@ -25,7 +24,7 @@ def create_user(request):
         'title': 'Админ | Регистрация',
         'form': form,
     }
-    return render(request, 'adminapp/admin-users-create.html',context)
+    return render(request, 'adminapp/admin-users-create.html', context)
 
 
 def read_users(request):
@@ -35,8 +34,21 @@ def read_users(request):
     return render(request, 'adminapp/admin-users-read.html', context)
 
 
-def update_user(request):
-    return render(request, 'adminapp/admin-users-update-delete.html')
+def update_user(request, pk):
+    user_select = User.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, instance=user_select, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('adminapp:read_users'))
+    else:
+        form = UserAdminProfileForm(instance=user_select)
+    context = {
+        'title': 'Админ | Редактирование пользователя',
+        'form': form,
+        'user_select': user_select
+    }
+    return render(request, 'adminapp/admin-users-update-delete.html', context)
 
 
 def delete_user(request):
