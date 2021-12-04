c7e905c9e040a4dfa9f3eb3268af3a0caa748331
georgekorob <georgekorob@gmail.com>
2021-12-04 13:43:43 +0300

    create controllers

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from adminapp.views import index
+from adminapp.views import index, create_user, read_users, update_user, delete_user
 
 app_name = 'adminapp'
 urlpatterns = [
     path('', index, name='index'),
+    path('create_user', create_user, name='create_user'),
+    path('read_users', read_users, name='read_users'),
+    path('update_user/<int:pk>', update_user, name='update_user'),
+    path('delete_user/<int:pk>', delete_user, name='delete_user'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -2,5 +2,27 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from authapp.models import User
+
+
 def index(request):
     return render(request, 'adminapp/admin.html')
+
+
+def create_user(request):
+    return render(request, 'adminapp/admin-users-create.html')
+
+
+def read_users(request):
+    context = {
+        'users': User.objects.all()
+    }
+    return render(request, 'adminapp/admin-users-read.html', context)
+
+
+def update_user(request):
+    return render(request, 'adminapp/admin-users-update-delete.html')
+
+
+def delete_user(request):
+    return render(request, 'adminapp/admin-users-read.html')
