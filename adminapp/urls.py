a110a31fa9b9428438319d3642ac2872291cd644
georgekorob <georgekorob@gmail.com>
2021-12-04 14:54:32 +0300

    add read category

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from adminapp.views import index, create_user, read_users, update_user, delete_user
+from adminapp.views import create_category, read_categories, update_category, delete_category
 
 app_name = 'adminapp'
 urlpatterns = [
@@ -8,4 +9,8 @@ urlpatterns = [
     path('read_users', read_users, name='read_users'),
     path('update_user/<int:pk>', update_user, name='update_user'),
     path('delete_user/<int:pk>', delete_user, name='delete_user'),
+    path('create_category', create_category, name='create_category'),
+    path('read_categories', read_categories, name='read_categories'),
+    path('update_category/<int:pk>', update_category, name='update_category'),
+    path('delete_category/<int:pk>', delete_category, name='delete_category'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 
 from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 from authapp.models import User
+from mainapp.models import ProductCategory
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -63,3 +64,55 @@ def delete_user(request, pk):
         user.is_active = not user.is_active
         user.save()
     return HttpResponseRedirect(reverse('adminapp:read_users'))
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def create_category(request):
+    pass
+    # if request.method == 'POST':
+    #     form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+    #     if form.is_valid():
+    #         form.save()  # fdkebov427hflk
+    #         return HttpResponseRedirect(reverse('adminapp:read_users'))
+    # else:
+    #     form = UserAdminRegisterForm()
+    # context = {
+    #     'title': 'Админ | Регистрация',
+    #     'form': form,
+    # }
+    # return render(request, 'adminapp/admin-users-create.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def read_categories(request):
+    context = {
+        'categories': ProductCategory.objects.all()
+    }
+    return render(request, 'adminapp/admin-category-read.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def update_category(request, pk):
+    pass
+    # user_select = User.objects.get(pk=pk)
+    # if request.method == 'POST':
+    #     form = UserAdminProfileForm(data=request.POST, instance=user_select, files=request.FILES)
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect(reverse('adminapp:read_users'))
+    # else:
+    #     form = UserAdminProfileForm(instance=user_select)
+    # context = {
+    #     'title': 'Админ | Редактирование пользователя',
+    #     'form': form,
+    #     'user_select': user_select
+    # }
+    # return render(request, 'adminapp/admin-users-update-delete.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def delete_category(request, pk):
+    if request.method == 'POST':
+        category = ProductCategory.objects.get(pk=pk)
+        category.delete()
+    return HttpResponseRedirect(reverse('adminapp:read_categories'))
