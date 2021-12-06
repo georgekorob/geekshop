93f26e4f3e94a97a0943e7accb1a56539c1c5a01
George <georgekorob@gmail.com>
2021-12-07 01:57:17 +0300

    basket view

--- basketapp/views.py
+++ basketapp/views.py
@@ -51,16 +51,3 @@ def basket_edit(request, id, quantity):
                    'total_quantity': total_quantity, }
         result = render_to_string('basketapp/basket.html', context)
         return JsonResponse({'result': result})
-
-# @login_required
-# def basket_add(request, id):
-#     user_select = request.user
-#     product = Product.objects.get(id=id)
-#     baskets = Basket.objects.filter(user=user_select, product=product)
-#     if baskets:
-#         basket = baskets.first()
-#         basket.quantity += 1
-#         basket.save()
-#     else:
-#         Basket.objects.create(user=user_select, product=product, quantity=1)
-#     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
