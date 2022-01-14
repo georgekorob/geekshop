d6606f94a866e0e476a62d3cab22893f01873ea9
George <georgekorob@gmail.com>
2022-01-14 11:05:05 +0300

    get_summary total from order product str() delete category

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -30,17 +30,24 @@ class Order(models.Model):
     def __str__(self):
         return f'Текущий заказ {self.pk}'
 
-    @property
-    def get_total_quantity(self):
-        return sum(list(map(lambda x: x.quantity, self.get_items)))
+    # @property
+    # def get_total_quantity(self):
+    #     return sum(list(map(lambda x: x.quantity, self.get_items)))
+    #
+    # @property
+    # def get_total_cost(self):
+    #     return sum(list(map(lambda x: x.get_product_cost, self.get_items)))
+    #
+    # @property
+    # def get_items(self):
+    #     return self.orderitems.select_related()
 
-    @property
-    def get_total_cost(self):
-        return sum(list(map(lambda x: x.get_product_cost, self.get_items)))
-
-    @property
-    def get_items(self):
-        return self.orderitems.select_related()
+    def get_summary(self):
+        items = self.orderitems.select_related()
+        return {
+            'get_total_quantity': sum(list(map(lambda x: x.quantity, items))),
+            'get_total_cost': sum(list(map(lambda x: x.get_product_cost, items))),
+        }
 
     def delete(self, using=None, keep_parents=False):
         for item in self.get_items:
